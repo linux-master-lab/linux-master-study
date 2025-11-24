d39269ca72406422069faaacc01c7702a59d9d13
stella0306 <starstella22222@gmail.com>
2025-11-24 10:04:09 +0900

    feat: section5까지 일부 작성

--- src/components/pages/sections/components/Section4.js
+++ src/components/pages/sections/components/Section4.js
@@ -6,13 +6,104 @@ import "../../../styles/sections/Sections.css";
 export function Section4() {
     return (
         <SectionTemplate title="리눅스 기본 설치 및 유형">
-            <h2 className="section-subtitle">부제목</h2>
 
-            <SectionBlock title="소제목">
+            {/* 부제목 */}
+            <h2 className="section-subtitle">리눅스 설치의 개요</h2>
+
+            {/* 내용 */}
+            <SectionBlock title="내용">
                 <ul className="section-list">
-                    <li>내용1</li>
+                    <li>리눅스 설치 파일은 해당 배포본의 홈페이지에서 다운로드 받을 수 있다.</li>
+                    <li>리눅스는 단 하나의 제품 또는 한 종류의 제품군만 있는 것이 아니다.</li>
+                    <li>리눅스는 배포본마다 설치 환경과 설치 과정이 다르다.</li>
+                    <li>리눅스의 설치 유형은 배포판마다 다르지만 패키지에 따라 데스크탑형, 서버형, 사용자 정의형으로 구분된다.</li>
+                    <li>설치 전에 시스템에 있는 모든 파일을 백업해 둔다.</li>
+                    <li>멀티 부팅 시스템을 만든다면, 현재 운영체제의 배포 미디어를 가지고 있어야 한다.</li>
+                    <li>부팅 드라이브를 다시 파티션 하는 경우라면, 운영체제의 부트로더를 다시 설치해야 할 수도 있고, 더 많은 경우에 운영체제 전체를 해당
+                        파티션에 다시 설치해야한다.</li>
                 </ul>
             </SectionBlock>
+
+            {/* 부제목 */}
+            <h2 className="section-subtitle">리눅스 설치를 위한 하드웨어 정보 파악</h2>
+
+            {/* 내용 */}
+            <SectionBlock title="내용">
+                <ul className="section-list">
+                    <li>하드웨어 정보</li>
+                    <li>하드웨어 호환성</li>
+                    <li>네트워크 설정</li>
+                </ul>
+            </SectionBlock>
+
+            {/* 부제목 */}
+            <h2 className="section-subtitle">리눅스 설치하기</h2>
+
+            {/* 내용 */}
+            <SectionBlock title="내용">
+                <ul className="section-info-list">
+                    <li className="section-info-item">
+                        <span className="section-info-name">1단계</span>
+                        <span className="section-info-desc">설치 초기화면 - Test this media & install CentOS7, Install CentOS8</span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">2단계</span>
+                        <span className="section-info-desc">설치 초기화면 - Rescue a CentOS System, Run a memory test</span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">3단계</span>
+                        <span className="section-info-desc">언어 선택</span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">4단계</span>
+                        <span className="section-info-desc">설치 요약 확인</span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">5단계</span>
+                        <span className="section-info-desc">날짜와 시간 설정</span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">6단계</span>
+                        <span className="section-info-desc">설치 소스 - 저장소(repository) 서버 주소 변경</span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">7단계</span>
+                        <span className="section-info-desc">설치 소스 - 설치와 관련된 특수 저장 장치 설정</span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">8단계</span>
+                        <span className="section-info-desc">설치 소스 - 기본 저장 장치 하드디스크 파티션 설정</span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">9단계</span>
+                        <span className="section-info-desc">소프트웨어 선택</span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">10단계</span>
+                        <span className="section-info-desc">kdump 설정</span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">11단계</span>
+                        <span className="section-info-desc">네트워크 설정과 호스트명 지정</span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">12단계</span>
+                        <span className="section-info-desc">패스워드 지정</span>
+                    </li>
+                </ul>
+            </SectionBlock>
+
         </SectionTemplate>
     );
 }
--- src/components/pages/sections/components/Section5.js
+++ src/components/pages/sections/components/Section5.js
@@ -6,13 +6,136 @@ import "../../../styles/sections/Sections.css";
 export function Section5() {
     return (
         <SectionTemplate title="리눅스 파티션">
-            <h2 className="section-subtitle">부제목</h2>
 
-            <SectionBlock title="소제목">
+            {/* 부제목 */}
+            <h2 className="section-subtitle">파티션의 특징과 종류</h2>
+
+            {/* 내용 */}
+            <SectionBlock title="내용">
+
+                {/* 특징 */}
                 <ul className="section-list">
-                    <li>내용1</li>
+                    <li>파티션이란, 하나의 물리적 디스크를 여러 개의 논리적인 디스크로 분활하는 것이다.</li>
+                    <li>파티션은 주 파티션, 확장 파티션, 논리 파티션, 스왑 파티션으로 구분된다.</li>
+                </ul>
+
+                {/* 파티션 */}
+                <ul className="section-info-list">
+                    <li className="section-info-item">
+                        <span className="section-info-name">주 파티션</span>
+                        <span className="section-info-desc">
+                            부팅이 가능한 기본 파티션<br/>
+                            하나의 하드디스크에 최대 4개의 주 파티션 분활 가능<br/>
+                            하드디스크를 4개 이상의 파티션으로 사용해야 할 때 하나의 확장 파티션을 설정하여 확장 파티션 안에 여러 개의 논리 파티션을 분할하여 데이터
+                            저장
+                        </span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">확장 파티션</span>
+                        <span className="section-info-desc">
+                            주 파티션 내에 생성, 하나의 물리적 디스크에 1개만 생성<br/>
+                            파티션 번호는 1~4번이 할당<br/>
+                            데이터 저장 영역을 위한 것이 아니라 논리 파티션을 생성
+                        </span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">논리 파티션</span>
+                        <span className="section-info-desc">
+                            확장 파티션 안에 생성되는 파티션<br/>
+                            논리 파티션은 12개 이상 생성하지 않는 것을 권고하며 5번 이후의 번호가 붙여짐
+                        </span>
+                    </li>
+
+                    <li className="section-info-item">
+                        <span className="section-info-name">스왑 파티션</span>
+                        <span className="section-info-desc">
+                            하드디스크의 일부를 메모리처럼 사용하는 영역<br/>
+                            주 파티션 또는 논리 파티션에 생성<br/>
+                            프로그램 실행 시 부족한 메모리 용량을 하드디스크로 리눅스 설치시에 반드시 설치되어야 하는 영역<br/>
+                            스왑 영역의 크기는 메모리의 2배를 설정하도록 권고
+                        </span>
+                    </li>
+
                 </ul>
             </SectionBlock>
+
+            {/* 부제목 */}
+            <h2 className="section-subtitle">디스크와 장치명</h2>
+
+            {/* 내용 */}
+            <SectionBlock title="내용">
+                <ul className="section-list">
+                    <li>분활된 파티션은 디스크의 장치 파일명 뒤에 숫자를 붙인다.</li>
+                    <li>리눅스에서 파티션을 만들고 마운트 할 때 지정된 디바이스명을 사용한다.</li>
+                </ul>
+            </SectionBlock>
+
+            {/* 부제목 */}
+            <h2 className="section-subtitle">파일 시스템</h2>
+
+            {/* 내용 */}
+            <SectionBlock title="내용">
+                <ul className="section-list">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </SectionBlock>
+
+            {/* 부제목 */}
+            <h2 className="section-subtitle">LVM(Logical Volumn Manager)</h2>
+
+            {/* 내용 */}
+            <SectionBlock title="내용">
+                <ul className="section-list">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </SectionBlock>
+
+            {/* 부제목 */}
+            <h2 className="section-subtitle">RAID</h2>
+
+            {/* 내용 */}
+            <SectionBlock title="내용">
+                <ul className="section-list">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </SectionBlock>
+
+            {/* 부제목 */}
+            <h2 className="section-subtitle">파티션 분활</h2>
+
+            {/* 내용 */}
+            <SectionBlock title="내용">
+                <ul className="section-list">
+                    <li></li>
+                    <li></li>
+                </ul>
+            </SectionBlock>
+
+            {/* 부제목 */}
+            <h2 className="section-subtitle">디스크와 장치명</h2>
+
+            {/* 내용 */}
+            <SectionBlock title="내용">
+                <ul className="section-list">
+                    <li></li>
+                    <li></li>
+                </ul>
+            </SectionBlock>
+
         </SectionTemplate>
     );
 }
