02c5483389319afa106411de5b977bce21b1ef8b
stella0306 <starstella22222@gmail.com>
2025-11-26 15:58:45 +0900

    feat: Sction2 작성

--- src/components/pages/sections2/SectionList2.js
+++ src/components/pages/sections2/SectionList2.js
@@ -1,6 +1,6 @@
 import { SectionHeader } from "./components/SectionHeader";
 import { Section1 } from "./components/Section1";
-// import { Section2 } from "./components/Section2";
+import { Section2 } from "./components/Section2";
 // import { Section3 } from "./components/Section3";
 // import { Section4 } from "./components/Section4";
 // import { Section5 } from "./components/Section5";
@@ -15,8 +15,8 @@ export default function SectionList2() {
     <div className="sections-wrapper">
       <SectionHeader />
       <Section1 />
-      {/* <Section2 />  
-      <Section3 />
+      <Section2 />  
+      {/* <Section3 />
       <Section4 />
       <Section5 />
       <Section6 />
