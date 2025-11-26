b0d83efae26468e58f9644c18ddd31b0231c0ad5
stella0306 <starstella22222@gmail.com>
2025-11-26 16:29:31 +0900

    feat: Sction4 작성

--- src/components/pages/sections2/SectionList2.js
+++ src/components/pages/sections2/SectionList2.js
@@ -2,7 +2,7 @@ import { SectionHeader } from "./components/SectionHeader";
 import { Section1 } from "./components/Section1";
 import { Section2 } from "./components/Section2";
 import { Section3 } from "./components/Section3";
-// import { Section4 } from "./components/Section4";
+import { Section4 } from "./components/Section4";
 // import { Section5 } from "./components/Section5";
 // import { Section6 } from "./components/Section6";
 // import { Section7 } from "./components/Section7";
@@ -17,7 +17,7 @@ export default function SectionList2() {
       <Section1 />
       <Section2 />  
       <Section3 />
-      {/* <Section4 /> */}
+      <Section4 />
       {/* <Section5 /> */}
       {/* <Section6 /> */}
       {/* <Section7 /> */}
