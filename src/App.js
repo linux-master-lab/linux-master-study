95c06f902b354e1a2be45a53a5cd7944728c63d1
stella0306 <starstella22222@gmail.com>
2025-12-07 23:32:53 +0900

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, HashRouter, Routes, Route} from "react-router-dom";
 import SectionList1 from './components/pages/sections1/SectionList1';
 import SectionList2 from './components/pages/sections2/SectionList2';
 import SectionList3 from './components/pages/sections3/SectionList3';
@@ -7,7 +7,7 @@ import SectionList3 from './components/pages/sections3/SectionList3';
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     {/* 기본 경로 */}
                     {/* <Route path="/" element={<Sections />}/> */}
@@ -21,7 +21,7 @@ function App() {
                     {/* 리눅스 웹 */}
                     <Route path="/liunx-list3" element={<SectionList3 />}/>
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 }
